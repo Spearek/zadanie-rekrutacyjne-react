3db075744876ed98e874630d7dbaecbd89e6b802
Spearek <30389214+Spearek@users.noreply.github.com>
2019-01-06 22:49:17 +0100

    pets fav food visiblity implemented

--- src/components/Pets/Pet/Pet.js
+++ src/components/Pets/Pet/Pet.js
@@ -4,6 +4,19 @@ import '../../../assets/photoshop slices/delete.png'
 
 const pet = (props)=> {
 
+    let favorites = null;
+
+    if(props.petDetails.favFoods !== undefined){
+        favorites = (
+           <div className="favFoodContainer">
+               <p>Ulubione jedzenie:</p>
+               {props.petDetails.favFoods.map(value=>{
+                   return <p className="dish">{value}</p>
+               })}
+           </div>     
+            )
+    
+    }
     return(
         <div className="petInfo">
             <img src={props.petDetails.photo} alt={props.petDetails.species}/>
@@ -11,8 +24,9 @@ const pet = (props)=> {
                 <p>Imię: {props.petDetails.name}</p>
                 <p>Wiek: {2019 - props.petDetails.birthYear}</p>
                 <p>Gatunek: {props.petDetails.species}</p>
+                {favorites}
             </div>
-            <img className="close" src={require('../../../assets/photoshop slices/delete.png')} alt='xxx' onClick={props.click} />
+            <img className="close" src={require('../../../assets/photoshop slices/delete.png')} alt='delete element' onClick={props.click} />
         </div>
         
     )
