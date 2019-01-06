36c0395fafe26bd4c103267cd9de786def4985f6
Spearek <30389214+Spearek@users.noreply.github.com>
2019-01-06 21:02:41 +0100

    delete layout element added

--- src/components/Pets/Pet/Pet.js
+++ src/components/Pets/Pet/Pet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Pet.css';
+import '../../../assets/photoshop slices/delete.png'
 
 const pet = (props)=> {
 
@@ -13,10 +14,11 @@ const pet = (props)=> {
         <div className="petInfo">
             <img src={props.petDetails.photo} alt={props.petDetails.species}/>
             <div className="petdetails">
-            <p>Imię: {props.petDetails.name}</p>
-            <p>Wiek: {2019 - props.petDetails.birthYear}</p>
-            <p>Gatunek: {props.petDetails.species}</p>
+                <p>Imię: {props.petDetails.name}</p>
+                <p>Wiek: {2019 - props.petDetails.birthYear}</p>
+                <p>Gatunek: {props.petDetails.species}</p>
             </div>
+            <img className="close" src={require('../../../assets/photoshop slices/delete.png')} alt='xxx' />
         </div>
         
     )
